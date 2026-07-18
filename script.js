347b0e278ed6127bdbe6225f2e1f7beae76572f2
Cadencetryingtocode14 <ca083083@ucf.edu>
2026-07-17 22:40:11 -0400

    fixing js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const themeButton = document.getElementById("themeToggle");
+
 themeButton.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
+    console.log("button clicked");
+
     if (document.body.classList.contains("dark-mode")) {
         themeButton.textContent = "Light Mode";
     } else {
